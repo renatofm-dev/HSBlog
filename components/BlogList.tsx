02793e86737bf14624cf4b82588cc0de0591ea91
Renato Martins <rmartins6694@gmail.com>
2023-01-28 11:12:47 -0300

    add new lib: @tailwind line-clamp, create component ClientSideROute and add description in schema

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 import urlFor from "../lib/urlFor"
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid"
+import ClientSideRoute from "./ClientSideRoute"
 
 type Props = {
   posts: Post[],
@@ -10,10 +12,11 @@ function BlogList({ posts }: Props) {
     <div>
       <hr className="border-[#F7AB0A] mb-10" />
 
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gao-y-16 pb-24">
         {/*posts*/}
         {posts.map(post => (
-          <div key={post._id} className="flex flex-col group cursor-pointer">
+          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+          <div  className="flex flex-col group cursor-pointer">
             <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
               <Image
                 className="object-cover object-left lg:object-center"
@@ -38,19 +41,25 @@ function BlogList({ posts }: Props) {
                   </div>
                   <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
                     {post.categories.map(category => (
-                      <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                      <div className="line-clamp-2 bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
                         <p>{category.title}</p>
                       </div>
                     ))}
                   </div>
-
                 </div>
-
-              
-              
             </div>
+              
+              <div className="line-clamp-3 mt-5 flex-1">
+                <p className="underline text-lg font-bold">{post.title}</p>
+                <p className="text-gray-500">{post.description}</p>
+              </div>
+              <p className="mt-5 font-bold flex items-center group-hover:underline">
+                Read Post
 
+                <ArrowUpRightIcon  className="ml-2 h-4 w-4"/>
+              </p>
           </div>
+          </ClientSideRoute>
         ))}
       </div>
     </div>
