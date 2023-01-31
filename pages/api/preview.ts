3e01a49a881224e94b8aaa7bd9c59950e8a0c69d
Renato Martins <rmartins6694@gmail.com>
2023-01-31 07:37:32 -0300

    changes in preview.ts

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,11 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import Post from "../../app/(user)/post/[slug]/page";
 
-type Props = {
-    params: {
-        slug: string;
-    }
-  };
+
 
 
 export default function preview(req: NextApiRequest, res: NextApiResponse) {
