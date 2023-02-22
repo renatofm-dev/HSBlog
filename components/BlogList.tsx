1718e7c2bdad3be7b769306a629788d725913352
Renato Martins <rmartins6694@gmail.com>
2023-02-22 18:20:22 -0300

    change line-clamp in BlogList.tsx

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -12,12 +12,12 @@ function BlogList({ posts }: Props) {
     <div>
       <hr className="border-[#F7AB0A] mb-10" />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gao-y-16 pb-24">
+      <div className="grid grid-cols-1 gap-10 px-10 pb-24 md:grid-cols-2 gao-y-16">
         {/*posts*/}
         {posts.map(post => (
           <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
-          <div  className="flex flex-col group cursor-pointer">
-            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+          <div  className="flex flex-col cursor-pointer group">
+            <div className="relative w-full transition-transform duration-200 ease-out h-80 drop-shadow-xl group-hover:scale-105">
               <Image
                 className="object-cover object-left lg:object-center"
                 src={urlFor(post.mainImage).url()}
@@ -26,7 +26,7 @@ function BlogList({ posts }: Props) {
               />
 
               
-                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                <div className="absolute bottom-0 flex justify-between w-full p-5 text-white bg-black rounded bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
                   <div>
                     <p className="font-bold">{post.title}</p>
                     <p>
@@ -39,9 +39,9 @@ function BlogList({ posts }: Props) {
                       )}
                     </p>
                   </div>
-                  <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                  <div className="flex flex-col items-center md:flex-row gap-y-2 md:gap-x-2">
                     {post.categories.map(category => (
-                      <div className="line-clamp-2 bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                      <div className="line-clamp-none bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
                         <p>{category.title}</p>
                       </div>
                     ))}
@@ -49,13 +49,13 @@ function BlogList({ posts }: Props) {
                 </div>
             </div>
               
-              <div className="line-clamp-3 mt-5 flex-1">
-                <p className="underline text-lg font-bold">{post.title}</p>
+              <div className="flex-1 mt-5 line-clamp-3">
+                <p className="text-lg font-bold underline">{post.title}</p>
                 <p className="text-gray-500">{post.description}</p>
               </div>
-              <p className="mt-5 font-bold flex items-center group-hover:underline">
+              <p className="flex items-center mt-5 font-bold group-hover:underline">
                 Leia o Post
-                <ArrowUpRightIcon  className="ml-2 h-4 w-4"/>
+                <ArrowUpRightIcon  className="w-4 h-4 ml-2"/>
               </p>
           </div>
           </ClientSideRoute>
